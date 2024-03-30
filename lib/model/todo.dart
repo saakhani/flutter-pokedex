24261ac87e84a5bafab7ceaaa420e19f66d77730
Saad Lakhani <muhammad.saad.002002@gmail.com>
2024-03-30 14:18:17 +0500

    Add Todo model and TodoService class

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -2,7 +2,12 @@ class Todo {
   final int id;
   final int userId;
   final String title;
-  final bool completed
+  final bool completed;
+
+  Todo({
+    required this.id,
+    required this.userId,
+    required this.title,
+    required this.completed,
+  });
 }
-
-
